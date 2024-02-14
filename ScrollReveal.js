86e61360ef0fedf84ef03263d86c1c389eb7835a
Augusto Finotti <augustofoliver@gmail.com>
2024-02-14 00:26:36 -0300

    Construção do menu, animação de página em desenvolvimento e footer.

---
+++ ScrollReveal.js
@@ -0,0 +1,13 @@
+// Scroll Reveal
+window.revealing = ScrollReveal({reset:true})
+// Title Reaveling
+revealing.reveal('.TitleScroll',{
+    duration: 1500,
+    distance: '50px'
+})
+// Terminal Reaveling
+revealing.reveal('.TerminalScroll',{
+    duration: 1500,
+    distance: '50px',
+    delay: 750
+})
